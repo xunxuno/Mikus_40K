09811b7bf581e2a707cb75597630e7b251942e66
WebiWaboK <pepin21112003@gmail.com>
2024-10-30 18:15:29 -0600

    Empezando el front

--- Mikus_40K/src/components/router.tsx
+++ Mikus_40K/src/components/router.tsx
@@ -13,15 +13,7 @@ const routes = {
     login: '/login',
 };
 
-// Home component
-function Home() {
-    return (
-        <div>
-            <h1>MIKUS 40K</h1>
-            <Link to={routes.login}>Sing Up</Link>
-        </div>
-    );
-}
+
 
 function Router(){
     return (
