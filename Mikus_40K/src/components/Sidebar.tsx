cdf10d469b2f69b93e16af67433909617a5febd7
WebiWaboK <pepin21112003@gmail.com>
2024-11-09 18:17:33 -0600

    Sidebar and Carrousel fixed!

---
+++ Mikus_40K/src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="sidebar">
+      <p className="sidebar-text">Sidebar</p>
+    </div>
+  );
+};
+
+export default Sidebar;
--- Mikus_40K/src/components/home.tsx
+++ Mikus_40K/src/components/home.tsx
@@ -4,6 +4,7 @@ import image1 from '../images/HALLOWEEN(1).png';
 import image2 from '../images/HALLOWEEN.png';
 import ImageCarousel from './ImageCarousel';
 import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 
 const images = [image1, image2];
@@ -15,6 +16,7 @@ function Home() {
         <div>
             <Navbar />
             <ImageCarousel images={images} />
+            <Sidebar />
         </div>
     );
 }
