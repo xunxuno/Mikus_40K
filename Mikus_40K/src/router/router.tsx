c3a63dba9d8135c3bdabc52cd7ef4f00df7527e7
FranciscoOlivaresFerrae <folivaresferraez@gmail.com>
2024-11-20 23:15:46 -0600

    FooterFix

--- Mikus_40K/src/router/router.tsx
+++ Mikus_40K/src/router/router.tsx
@@ -21,29 +21,32 @@ import Register from "../components/Login/register";
 
 function Router() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/help-center" element={<HelpCenter />} />
-            <Route path="/contact-support" element={<ContactSupport />} />
-            <Route path="/about-us" element={<AboutUs/>} />
-            <Route path="/catalog" element={<Catalog/>} />
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="/purchase-history" element={<PurchaseHistory/>} />
-            <Route path="/user-points" element={<Points/>} />
-            <Route path="/change-password" element={<ChangePass/>} />
-            <Route path="/payment-method" element={<PaymentMethod/>} />
-            <Route path="/shipping-addresses" element={<ShippingAddresses/>} />
-            <Route path="/cart" element={<Cart/>} />
-            <Route path="/wishlist" element={<WishList/>} />
-            <Route path="/orders" element={<Orders/>} />
-            <Route path="/track-order" element={<TrackOrder/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Register/>} />
-            <Route path="*" element={<Navigate to="/" />} />
-            
-        </Routes>
+        <div className="router-container">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/help-center" element={<HelpCenter />} />
+                <Route path="/contact-support" element={<ContactSupport />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/catalog" element={<Catalog />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/purchase-history" element={<PurchaseHistory />} />
+                <Route path="/user-points" element={<Points />} />
+                <Route path="/change-password" element={<ChangePass />} />
+                <Route path="/payment-method" element={<PaymentMethod />} />
+                <Route path="/shipping-addresses" element={<ShippingAddresses />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/wishlist" element={<WishList />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/track-order" element={<TrackOrder />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </div>
     );
 }
 
+
 export default Router;
+
