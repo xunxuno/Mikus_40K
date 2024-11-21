5fef39e3118aaadf0d07f9a5fd85d9744acbb4c1
xunxuno <sintraxlch@gmail.com>
2024-11-21 00:57:23 -0600

    login and sing up ready

--- Mikus_40K/.history/src/components/Login/login_20241120035353.tsx
+++ Mikus_40K/.history/src/components/Login/login_20241120035353.tsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import './login.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';  // Importa el tipo de estado global
-import { logearUsuario_ } from '../../controllers/userController';  // Importa la función corregida
+import { loginUser } from '../../controllers/authController';
 import { useNavigate } from 'react-router-dom';  // Importa useNavigate
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState<string>('');  // Cambié `username` a `email` para que coincida con la función
+  const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [message, setMessage] = useState<string>('');
   const [messageType, setMessageType] = useState<string>('');
@@ -15,35 +15,18 @@ const Login: React.FC = () => {
   const navigate = useNavigate();  // Inicializa useNavigate
   const { username: loggedUsername, token } = useSelector((state: RootState) => state.auth);
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (email && password) {
-      try {
-        // Llamamos a la función logearUsuario_ con email y password
-        const response = await logearUsuario_(email, password);
-        
-        // Si la respuesta tiene result, significa que el login fue exitoso
-        if (response.result) {
-          setMessage('¡Inicio de sesión exitoso!');
-          setMessageType('success');
-          
-          // Aquí puedes manejar el guardado del token y el userId en el estado global si es necesario
-          // dispatch(setUser({ token: response.result.token, userId: response.result.userId }));
+    if (username && password) {
+      loginUser(dispatch, username, password);  // Se pasa también la contraseña
+      setMessage('¡Inicio de sesión exitoso!');
+      setMessageType('success');
 
-          // Redirige a la página de inicio ("/")
-          navigate('/');  
-        } else {
-          setMessage(response.mensaje);  // Muestra el mensaje de error
-          setMessageType('error');
-        }
-      } catch (error) {
-        console.error('Error al intentar logear:', error);
-        setMessage('Hubo un error al intentar logear el usuario');
-        setMessageType('error');
-      }
+      // Redirige a la página de inicio ("/")
+      navigate('/');  // Redirige al inicio
     } else {
-      setMessage('Por favor, ingresa un email y una contraseña');
+      setMessage('Usuario o contraseña incorrectos');
       setMessageType('error');
     }
   };
@@ -53,12 +36,12 @@ const Login: React.FC = () => {
       <div className="login">
         <h3>Inicia sesión</h3>
         <form onSubmit={handleSubmit} className="square_login">
-          <p>Email: </p>
+          <p>Usuario: </p>
           <input 
-            type="email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            placeholder="Ingresa tu email" 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            placeholder="Ingresa tu usuario" 
           />
           <p>Contraseña: </p>
           <input 
