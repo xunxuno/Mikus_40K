be976b49f4702ede838c102c1bdafc72117df52f
WebiWaboK <pepin21112003@gmail.com>
2024-12-04 15:38:00 -0600

    Routes

---
+++ Mikus_40K/src/contexts/AuthContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useState } from 'react';
+
+interface AuthContextProps {
+    isAuthenticated: boolean;
+    login: (token: string) => void;
+    logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(() => {
+        // Inicializa el estado leyendo desde localStorage
+        return localStorage.getItem('token') !== null;
+    });
+
+    const login = (token: string) => {
+        localStorage.setItem('token', token);
+        setIsAuthenticated(true);
+    };
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setIsAuthenticated(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth debe ser usado dentro de un AuthProvider');
+    }
+    return context;
+};
